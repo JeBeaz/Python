9dfbd89606be45b2426b02f4a011e426fc0f3e38
JeBeaz <Jeremybe@cbk12.com>
2023-02-09 06:55:14 -0800

    Chat GPT Testing

--- HourTracker.py
+++ HourTracker.py
@@ -1,39 +1,70 @@
-from datetime import datetime, timedelta
+import datetime
 
-def calculate_hours_worked(clock_in, lunch_duration, clock_out):
-    clock_in_time = datetime.strptime(clock_in, "%I:%M %p")
-    clock_out_time = datetime.strptime(clock_out, "%I:%M %p")
-    total_lunch_duration = timedelta(minutes=0)
-    for lunch in lunch_duration:
-        total_lunch_duration += timedelta(minutes=lunch)
-    return (clock_out_time - clock_in_time) - total_lunch_duration
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+work_week = {day: {'clock_in': None, 'clock_out': None, 'lunch_duration': 0} for day in days}
 
-days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-total_hours_worked = timedelta(minutes=0)
-for day_of_week in days_of_week:
-    clock_in = input("Enter clock-in time (HH:MM AM/PM) for {}: ".format(day_of_week))
-    lunch_durations = []
-    lunch = input("Enter lunch duration in minutes (or press enter to finish): ")
-    while lunch != "":
+def format_time(t):
+    hours, remainder = divmod(t.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return '{:02d}:{:02d}'.format(hours, minutes)
+
+def time_input(prompt):
+    while True:
+        time_str = input(prompt).strip()
         try:
-            lunch_duration = int(lunch)
+            if time_str:
+                time = datetime.datetime.strptime(time_str, '%I:%M %p').time()
+                return time
+            else:
+                return None
         except ValueError:
-            print("Invalid input, please enter a number.")
-            lunch = input("Enter lunch duration in minutes (or press enter to finish): ")
-            continue
-        lunch_durations.append(lunch_duration)
-        lunch = input("Enter lunch duration in minutes (or press enter to finish): ")
-    clock_out = input("Enter clock-out time (HH:MM AM/PM) for {}: ".format(day_of_week))
-    hours_worked = calculate_hours_worked(clock_in, lunch_durations, clock_out)
-    total_hours_worked += hours_worked
-    hours = int(hours_worked.total_seconds() / 3600)
-    minutes = int((hours_worked.total_seconds() / 60) % 60)
-    print("Hours worked on {}: {}:{:02d}".format(day_of_week, hours, minutes))
+            print("Invalid time format. Please enter time in the format 'HH:MM AM/PM'.")
 
-if day_of_week == "Friday":
-    hours = int(total_hours_worked.total_seconds() / 3600)
-    minutes = int((total_hours_worked.total_seconds() / 60) % 60)
-    print("Total hours worked this week: {}:{:02d}".format(hours, minutes))
+def lunch_duration_input(prompt):
+    while True:
+        lunch_duration_str = input(prompt).strip()
+        if not lunch_duration_str:
+            return 0
+        try:
+            lunch_duration = int(lunch_duration_str)
+            if lunch_duration >= 0:
+                return lunch_duration
+            else:
+                print("Invalid lunch duration. Lunch duration must be a positive integer.")
+        except ValueError:
+            print("Invalid lunch duration. Lunch duration must be a positive integer.")
+
+for day in days:
+    work_day = work_week[day]
+    print(f'{day}:')
+    clock_in = time_input("Enter clock in time (or press enter to skip): ")
+    if clock_in:
+        work_day['clock_in'] = clock_in
+        clock_out = time_input("Enter clock out time: ")
+        work_day['clock_out'] = clock_out
+        lunch_duration = lunch_duration_input("Enter lunch duration in minutes (or press enter to skip): ")
+        work_day['lunch_duration'] = lunch_duration
+    print()
+
+total_duration = datetime.timedelta()
+for day in days[:-1]:
+    work_day = work_week[day]
+    clock_in = work_day['clock_in']
+    clock_out = work_day['clock_out']
+    lunch_duration = work_day['lunch_duration']
+    if clock_in and clock_out:
+        clock_in_datetime = datetime.datetime.combine(datetime.date.today(), clock_in)
+        clock_out_datetime = datetime.datetime.combine(datetime.date.today(), clock_out)
+        work_duration = clock_out_datetime - clock_in_datetime - datetime.timedelta(minutes=lunch_duration)
+        total_duration += work_duration
+        print(f'{day}: {format_time(work_duration)}')
+
+hours_remaining = 40*60*60 - total_duration.total_seconds()
+hours_remaining = max(hours_remaining, 0)
+
+
+
+
 
 
 
--- Testing.py
+++ Testing.py
@@ -1,5 +1,57 @@
-import random
+import tkinter as tk
 
-dice = (0,20)
+def calculate_total_hours():
+    start_time = start_time_entry.get()
+    end_time = end_time_entry.get()
+
+    start_hour, start_minute = map(int, start_time.split(":"))
+    end_hour, end_minute = map(int, end_time.split(":"))
+    
+    if start_time.endswith("PM") and start_hour != 12:
+        start_hour += 12
+    if end_time.endswith("PM") and end_hour != 12:
+        end_hour += 12
+    if start_time.endswith("AM") and start_hour == 12:
+        start_hour = 0
+    if end_time.endswith("AM") and end_hour == 12:
+        end_hour = 0
+    
+    total_minutes = (end_hour - start_hour) * 60 + (end_minute - start_minute)
+    if lunch_duration.get():
+        total_minutes -= int(lunch_duration.get())
+
+    total_hours = total_minutes / 60
+    total_hours_string = f"{int(total_hours)}:{total_minutes % 60:02d}"
+    result_label.config(text=f"Total hours worked: {total_hours_string}")
+
+root = tk.Tk()
+root.geometry("1600x900")
+root.title("Hours Tracker")
+
+start_time_label = tk.Label(root, text="Start Time (HH:MM AM/PM):")
+start_time_label.pack()
+
+start_time_entry = tk.Entry(root)
+start_time_entry.pack()
+
+end_time_label = tk.Label(root, text="End Time (HH:MM AM/PM):")
+end_time_label.pack()
+
+end_time_entry = tk.Entry(root)
+end_time_entry.pack()
+
+lunch_duration_label = tk.Label(root, text="Lunch Duration (minutes):")
+lunch_duration_label.pack()
+
+lunch_duration = tk.Entry(root)
+lunch_duration.pack()
+
+calculate_button = tk.Button(root, text="Calculate", command=calculate_total_hours)
+calculate_button.pack()
+
+result_label = tk.Label(root)
+result_label.pack()
+
+root.mainloop()
 
 
